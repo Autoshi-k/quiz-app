796f316b0faa422efdc3c6aabe4e4cdf2730dd76
Autoshi-k <autoshigit@gmail.com>
2022-03-30 18:33:26 +0300

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="header"> 
+    <h3>Correct answres: <span>1/4</span></h3>
+    <button>Reset Count</button>
+  </div>
+</template>
+
+<style scoped>
+  .header {
+    width: 100vw;
+    height: 2.5rem;
+    display: flex;
+    align-items: center;
+    border-bottom: 2px solid palevioletred;
+  }
+  h3 {
+    color: rgb(218, 60, 152);
+    width: fit-content;
+    display: inline-block;
+    padding: 0 2rem;
+    font-weight: 600;
+  }
+</style>
